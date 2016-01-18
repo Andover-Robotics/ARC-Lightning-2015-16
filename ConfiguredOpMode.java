9c960f2d0fe17b2fdda6e667dc7b4e060603a091
Chairman Zhao <chairmanzhao42@gmail.com>
2016-01-18 13:16:51 -0700

    Create ConfiguredOpMode.java

---
+++ ConfiguredOpMode.java
@@ -0,0 +1,33 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Chairman Zhao on 1/18/2016.
+ */
+public class ConfiguredOpMode extends OpMode{
+    protected DcMotor dLeft, dRight, sLeft, sRight, sweep;                //The 'd' prefix indicates a drivetrain component, the 's' prefix indicates a slide component
+    protected Servo bRack, bTilt, hLeft, hRight;                          /*The 'b' prefix indicates a bucket component, the 'h' prefix indicates a hanger-manipulator
+                                                                          bRack = Rack and Pinion, bTilt = Servo for tilting bucket*/
+    //private final float JOY_PWR_MULT = (float) 0.8;
+    public void init(){                                                 //Basic initiallization of motor and servo objects
+        dLeft = hardwareMap.dcMotor.get("DL");
+        dRight = hardwareMap.dcMotor.get("DR");
+        sLeft = hardwareMap.dcMotor.get("SL");
+        sRight = hardwareMap.dcMotor.get("SR");
+        sweep = hardwareMap.dcMotor.get("S");
+       /* Don't initiallize servos for testing phase
+        bRack = hardwareMap.servo.get("R");
+        bTilt = hardwareMap.servo.get("T");
+        hLeft = hardwareMap.servo.get("HL");
+        hRight = hardwareMap.servo.get("HR");*/
+    }
+    public void loop(){
+
+    }
+    protected void jControl(DcMotor m){
+
+    }
+}
